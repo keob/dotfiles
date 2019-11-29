f4fc0654eeb4a71aee73a10c0fae3437157137b2
kevin <bugbinbo@outlook.com>
2019-11-29 10:13:42 +0800

    Add PowerShell config

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,17 @@
+# PowerShell parameter completion shim for the dotnet CLI 
+Register-ArgumentCompleter -Native -CommandName dotnet -ScriptBlock {
+     param($commandName, $wordToComplete, $cursorPosition)
+         dotnet complete --position $cursorPosition "$wordToComplete" | ForEach-Object {
+            [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+         }
+ }
+# Chocolatey profile
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
+
+Import-Module PoShFuck
+Import-Module posh-git 
+Import-Module oh-my-posh 
+Set-Theme Sorin

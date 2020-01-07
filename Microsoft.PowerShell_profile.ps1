ef084bd8fb42ddaeecd1d69dd1245f49b4d4fd03
keob <bugbinbo@outlook.com>
2020-01-07 16:15:25 +0800

    update

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,17 +1,9 @@
+Set-Alias py python.exe
+
 # PowerShell parameter completion shim for the dotnet CLI 
 Register-ArgumentCompleter -Native -CommandName dotnet -ScriptBlock {
-     param($commandName, $wordToComplete, $cursorPosition)
-         dotnet complete --position $cursorPosition "$wordToComplete" | ForEach-Object {
-            [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
-         }
- }
-# Chocolatey profile
-$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
-if (Test-Path($ChocolateyProfile)) {
-  Import-Module "$ChocolateyProfile"
+	param($commandName, $wordToComplete, $cursorPosition)
+        dotnet complete --position $cursorPosition "$wordToComplete" | ForEach-Object {
+        	[System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+        }
 }
-
-Import-Module PoShFuck
-Import-Module posh-git 
-Import-Module oh-my-posh 
-Set-Theme Sorin

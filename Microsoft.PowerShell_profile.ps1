1f00b7484830d975bc8bdba435b0ffaf65848a96
keob <kevinke2020@outlook.com>
2020-01-25 16:13:24 +0800

    update

--- Microsoft.PowerShell_profile.ps1
+++
@@ -1,9 +0,0 @@
-Set-Alias py python.exe
-
-# PowerShell parameter completion shim for the dotnet CLI 
-Register-ArgumentCompleter -Native -CommandName dotnet -ScriptBlock {
-	param($commandName, $wordToComplete, $cursorPosition)
-        dotnet complete --position $cursorPosition "$wordToComplete" | ForEach-Object {
-        	[System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
-        }
-}
---
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,2 @@
+Set-Alias venv 'C:\Users\kevin\venv\venv\Scripts\Activate.ps1'
+Set-Alias py 'python'

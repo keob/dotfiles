f7775d24e1a75fb737839b658fbdc1300a291586
keob <bugbinbo@outlook.com>
2020-02-23 21:34:39 +0800

    Add Git function to alias git command

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -1,2 +1,14 @@
+function GitStatus { git status }
+function GitCheckout { git checkout }
+function GitCommit { git commit }
+function GitAdd { git add }
+function GitAdds {git add . }
+
+Set-Alias gst GitStatus
+Set-Alias gct GitCommit
+Set-Alias gco GitCheckout
+Set-Alias ga GitAdd
+Set-Alias gaa GitAdds
+
 Set-Alias venv 'C:\Users\kevin\venv\venv\Scripts\Activate.ps1'
 Set-Alias py 'python'

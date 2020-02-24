ad4f9c4560fa2428ed15b7c2195d5714276ef3ca
keob <bugbinbo@outlook.com>
2020-02-24 22:00:38 +0800

    Add which killall command alias

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -6,6 +6,61 @@ function GitCommit { git commit }
 function GitAdd { git add }
 function GitAdds {git add . }
 
+function GoBack { Set-Location .. }
+
+function Set-CurrentWorkingDirectory
+{
+    param
+    (
+        $Path,
+        $LiteralPath,
+        $PassThru,
+        $StackName,
+        $UseTransaction
+    )
+    if ($Path -and ($Path.Contains('...')))
+    {
+        $a = [System.Text.RegularExpressions.Regex]::Split($Path, "(\.{3,})");
+        for ($i = 0; $i -lt $a.Length; $i++)
+        {
+            $e = $a[$i];
+            $l = $e.Length;
+            if (($l -gt 2) -and ($e -eq "".PadRight($l, '.')))
+            {
+                $a[$i] = ".." + [System.String]::Concat([System.Linq.Enumerable]::Repeat("\..", $l - 2))
+            }
+        }
+        $PSBoundParameters['Path'] = [System.String]::Concat($a)
+    }
+    return Set-Location @PSBoundParameters
+}
+
+function which
+{
+    $results =New-Object System.Collections.Generic.List[System.Object];
+    foreach ($command in $args)
+    {
+        $path = (Get-Command $command).Source
+        if ($path)
+        {
+            $results.Add($path);
+        }
+    }
+    return $results;
+}
+
+function killall
+{
+    $commands = Get-Process $args
+    foreach ($command in $commands)
+    {
+        Stop-Process $command.Id
+    }
+}
+
+Set-Alias .. GoBack
+Set-Alias cd Set-CurrentWorkingDirectory -Option "AllScope"
+
 Set-Alias gst GitStatus
 Set-Alias gct GitCommit
 Set-Alias gco GitCheckout

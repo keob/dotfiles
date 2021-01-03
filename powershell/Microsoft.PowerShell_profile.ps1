8fc46171ec6692a848099605b739e4ad08d71fcf
keob <isgopher@outlook.com>
2021-01-03 20:57:09 +0800

    update: powershell config

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -1,51 +1,3 @@
-function GitStatus{git status }
-function GitCheckout { git checkout $args }
-function GitCommit { git commit -m $args }
-function GitAdd { git add $args }
-
-function prompt {
-
-    $path = Split-Path -leaf -path (Get-Location)
-
-    Write-Host
-    Write-Host "# " -NoNewline -ForegroundColor Blue
-    Write-Host $env:USERNAME -NoNewline -ForegroundColor Cyan
-    Write-Host " in " -NoNewline
-    Write-Host $path" " -NoNewline -ForegroundColor Green
-    Write-Host ">" -NoNewline -ForegroundColor Magenta
-
-    return " " 
-}
-
-function GoBack { Set-Location .. }
-
-function Set-CurrentWorkingDirectory
-{
-    param
-    (
-        $Path,
-        $LiteralPath,
-        $PassThru,
-        $StackName,
-        $UseTransaction
-    )
-    if ($Path -and ($Path.Contains('...')))
-    {
-        $a = [System.Text.RegularExpressions.Regex]::Split($Path, "(\.{3,})");
-        for ($i = 0; $i -lt $a.Length; $i++)
-        {
-            $e = $a[$i];
-            $l = $e.Length;
-            if (($l -gt 2) -and ($e -eq "".PadRight($l, '.')))
-            {
-                $a[$i] = ".." + [System.String]::Concat([System.Linq.Enumerable]::Repeat("\..", $l - 2))
-            }
-        }
-        $PSBoundParameters['Path'] = [System.String]::Concat($a)
-    }
-    return Set-Location @PSBoundParameters
-}
-
 function which
 {
     $results =New-Object System.Collections.Generic.List[System.Object];
@@ -72,11 +24,6 @@ function killall
     }
 }
 
-function whoseport
-{
-    Get-NetTCPConnection -LocalPort $args
-}
-
 function rm-history
 {
 	Clear-History
@@ -88,11 +35,3 @@ function rm-history
 ${function:dt} = { Set-Location ~\Desktop }
 ${function:doc} = { Set-Location ~\Documents }
 ${function:dl} = { Set-Location ~\Downloads }
-
-Set-Alias .. GoBack
-Set-Alias cd Set-CurrentWorkingDirectory -Option "AllScope"
-
-Set-Alias gst GitStatus
-Set-Alias gct GitCommit
-Set-Alias gco GitCheckout
-Set-Alias ga GitAdd
